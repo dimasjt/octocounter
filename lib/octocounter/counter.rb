e8c135d5ec0122602b786a2eef0f64f15b8c9bd5
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-09-20 15:26:01 +0700

    refactor

--- lib/octocounter/counter.rb
+++ lib/octocounter/counter.rb
@@ -13,11 +13,12 @@ module Octocounter
     def calculate
       list = []
       Dir.glob(path + "**/*").select { |f| File.file?(f) }.each do |file|
+        file_path = File.open(file).path
         if matched = list.find { |item| FileUtils.compare_file(item[:file], file) }
           matched[:count] += 1
-          matched[:files] = "#{matched[:files]}\n#{File.open(file).path}"
+          matched[:files] = "#{matched[:files]}\n#{file_path}"
         else
-          list.push(files: File.open(file).path, file: file, count: 1)
+          list.push(files: file_path, file: file, count: 1)
         end
       end
       list
@@ -26,6 +27,7 @@ module Octocounter
     def print_to_screen
       rows = calculate.map do |item|
         content = File.open(item[:file]).read.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')[0..100]
+        content = content.scan(/.{40}/).join("\n")
         [item[:files], content, item[:count]]
       end
       puts Terminal::Table.new headings: ["Files", "Content", "Count"], rows: rows, style: { all_separators: true }
