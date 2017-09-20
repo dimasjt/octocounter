f67b92a721011263bf4725363bc1956c59679adf
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-09-20 16:38:36 +0700

    improve for big data size

--- lib/octocounter/counter.rb
+++ lib/octocounter/counter.rb
@@ -3,18 +3,23 @@ require "terminal-table"
 module Octocounter
   class Counter
     include Commander::Methods
-    attr_accessor :path
+
+    REGEX_PATH = %r{/\/$/}
+
+    attr_accessor :path, :list
 
     def initialize(path)
-      path = "#{path}/" unless path.match(/\/$/)
+      path = "#{path}/" unless path =~ REGEX_PATH
       @path = path
+      @list = []
     end
 
     def calculate
-      list = []
       Dir.glob(path + "**/*").select { |f| File.file?(f) }.each do |file|
-        file_path = File.open(file).path
-        if matched = list.find { |item| FileUtils.compare_file(item[:file], file) }
+        open_file = File.open(file)
+        file_path = open_file.path.sub(path, '')
+        open_file.close
+        if matched = matched?(file)
           matched[:count] += 1
           matched[:files] = "#{matched[:files]}\n#{file_path}"
         else
@@ -24,11 +29,32 @@ module Octocounter
       list
     end
 
+    def matched?(file)
+      list.find do |item|
+        file1, file2 = [File.open(file), File.open(item[:file])]
+        size1, size2 = [file1.size, file2.size]
+        file1.close
+        file2.close
+
+        size1 == size2 &&
+          FileUtils.compare_file(item[:file], file)
+      end
+    end
+
+    def content(item)
+      content = ""
+      index = 0
+      IO.foreach(item[:file]) do |line|
+        content += line
+        index += 1
+        break if index == 10
+      end
+      content.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')[0..50]
+    end
+
     def print_to_screen
       rows = calculate.map do |item|
-        content = File.open(item[:file]).read.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')[0..100]
-        content = content.scan(/.{40}/).join("\n")
-        [item[:files], content, item[:count]]
+        [item[:files], content(item), item[:count]]
       end
       puts Terminal::Table.new headings: ["Files", "Content", "Count"], rows: rows, style: { all_separators: true }
     end
--- test/octocounter/octocounter_test.rb
+++ test/octocounter/octocounter_test.rb
@@ -5,10 +5,10 @@ describe Octocounter::Counter do
     it "should count files by same content" do
       abcdef, diff = Octocounter::Counter.new("test/data").calculate
 
-      assert_equal abcdef[:files], "test/data/A/content2\ntest/data/B/D/content3\ntest/data/B/content1"
+      assert_equal abcdef[:files], "A/content2\nB/D/content3\nB/content1"
       assert_equal abcdef[:count], 3
 
-      assert_equal diff[:files], "test/data/B/D/diff"
+      assert_equal diff[:files], "B/D/diff"
       assert_equal diff[:count], 1
     end
   end
