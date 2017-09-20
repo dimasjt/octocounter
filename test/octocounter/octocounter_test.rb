2ef71ac0cb9453401adae39fedf5cb0e051573a6
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-09-20 17:32:35 +0700

    copy actual files test

--- test/octocounter/octocounter_test.rb
+++ test/octocounter/octocounter_test.rb
@@ -5,8 +5,8 @@ describe Octocounter::Counter do
     it "should count files by same content" do
       abcdef, diff = Octocounter::Counter.new("test/data").calculate
 
-      assert_equal abcdef[:files], "A/content2\nB/D/content3\nB/content1"
-      assert_equal abcdef[:count], 3
+      assert_equal abcdef[:files], "A/content2\nB/D/content3\nB/content1\ncontent1"
+      assert_equal abcdef[:count], 4
 
       assert_equal diff[:files], "B/D/diff"
       assert_equal diff[:count], 1
